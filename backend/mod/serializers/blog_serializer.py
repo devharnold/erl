8b81db0b344ecdb2143b271eddc17be3aff2514d
Henry Arnold <devharnold@gmail.com>
2024-06-13 22:27:04 +0300

    Update file: Blog serializer

--- backend/mod/serializers/blog_serializer.py
+++ backend/mod/serializers/blog_serializer.py
@@ -1,2 +1,13 @@
 from rest_framework import serializers
-from mod.models import 
+from mod.models import Blog
+
+class BlogSerializer(serializers.Serializer):
+    """"Serializer class for { Blog }object model"""
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+    def validate_title(self, value: str) -> str:
+        if not value.strip():
+            raise serializers.ValidationError("Title is blank")
+        return value
