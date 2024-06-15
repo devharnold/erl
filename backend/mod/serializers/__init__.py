a57c03f6c1ecd63a847c9674a26cb380e9cec825
Henry Arnold <devharnold@gmail.com>
2024-06-15 15:01:46 +0300

    update __init__.py file/serilizers dir

--- backend/mod/serializers/__init__.py
+++ backend/mod/serializers/__init__.py
@@ -1,3 +1,6 @@
 from .article_serializer import ArticleSerializer
 from .blog_serializer import BlogSerializer
 from .notification_serializer import NotificationSerializer
+from .post_serializer import PostSerializer
+
+__all__ = ['ArticleSerializer', 'BlogSerializer', 'NotificationSerializer', 'PostSerializer']
